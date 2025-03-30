6f4ee31f547a993fb6f5df2ef218e844ccb9a4bd
wallysands <sands224@umn.edu>
2025-03-30 15:20:40 -0500

    first

--- Assets/Scripts/MainPaintingAndReframingUI.cs
+++ Assets/Scripts/MainPaintingAndReframingUI.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Splines;
+using System.Linq;
 
 namespace IVLab.MinVR3
 {
@@ -39,6 +41,9 @@ namespace IVLab.MinVR3
             Debug.Assert(m_PaintMaterial != null);
 
             m_NumStrokes = 0;
+
+            // Debug.Log("Entering");
+            // m_NewArt = new List<Transform>();
         }
 
 
@@ -158,8 +163,213 @@ namespace IVLab.MinVR3
         public void Painting_OnExit()
         {
             m_NumStrokes++;
+            List<Vector3> currCenters = new List<Vector3>();
+
+            // Get center of the line.
+            for (int i = 0; i < m_CurrentStrokeBackVertices.Count; i+=2)
+            {
+                if (m_CurrentStrokeBackVertices.Count > i+1)
+                {
+                    Vector3 v0 = m_CurrentStrokeBackVertices[i];
+                    Vector3 v1 = m_CurrentStrokeBackVertices[i+1];
+                    currCenters.Add((v0 + v1) / 2);
+                    
+                    // Debug.Log("VERTICES LEFT LAST: " + v0 + " RIGHT LAST: " + v1);
+                }
+            }
+            currCenters = currCenters.Distinct().ToList();
+            // Debug.Log("CENTERS LAST: " + currCenters[^1] + " FIRST: " + currCenters[0]);
+            if (currCenters.Count > 0)
+            {
+                int splineIndex;
+                Spline spline = FindClosestSpline(currCenters, out splineIndex, out float splineStart, out float splineEnd);
+                Debug.Log("INDEX CHECK " +splineIndex);
+                BeginMorph(spline, splineIndex, splineStart, splineEnd);
+            }
         }
 
+        public Spline FindClosestSpline(List<Vector3> centers, out int bestIndex, out float splineStartIndex, out float splineEndIndex)
+        {
+            splineStartIndex = 0;
+            splineEndIndex = 1;
+            int w_dist = 10;
+            int w_dir = 1;
+            int sampleCount = 50;
+
+            // Spline conversion is currently slightly shifted, possible due to float precision. 
+            Spline drawingSpline = new Spline();
+            List<BezierKnot> knots = new List<BezierKnot>();
+            foreach (Vector3 c in centers)
+            {
+                knots.Add(new BezierKnot(c));
+                // drawingSpline.Add(new BezierKnot(c));
+                //knots.Add(new BezierKnot(new float3(x, y, z)));
+            }
+            drawingSpline.Knots = knots;
+            // m_SplineColoredContainer.AddSpline(drawingSpline);
+            // Debug.Log("DRAWING SPLINE: " + drawingSpline.EvaluatePosition(1));
+
+            Spline bestSpline = null;
+            float bestSimilaritySpline = float.MaxValue;
+            bestIndex = 0;
+            for (int index = 0; index < m_SplineContainer.Splines.Count; index++)
+            {
+                Spline spline = m_SplineContainer.Splines[index];
+                float splineSimilarity = 0;
+
+                float currStartIndex = 0;
+                float currEndIndex = sampleCount - 1;
+
+                // Loop through points evenly spaced along the drawn spline
+                for (int i = 0; i < sampleCount; i++)
+                {
+                    float t1 = i / (float)(sampleCount - 1); // Normalized parameter along spline
+                    Vector3 p1 = drawingSpline.EvaluatePosition(t1);
+                    Vector3 d1 = drawingSpline.EvaluateTangent(t1);
+
+                    ///// COMPARISON FROM EACH POINT TO ANYWHERE ALONG THE SPLINE, needs d2 calculation still
+                    // var distance = SplineUtility.GetNearestPoint(spline, p1, out var p2, out float _);
+                    
+                    // float similarity = w_dist * Mathf.Pow(distance, 2) + Mathf.Abs(w_dir * Vector3.Dot(d1, d2)); 
+
+                    // splineSimilarity += similarity;
+                    ///// END COMPARISON
+
+
+                    ///// COMPARISON FROM EACH POINT TO ANY POINT ON THE SPLINE
+
+
+                    float bestSimilarityPoint = float.MaxValue;
+                    // Loop through points evenly spaced along the current spline comparison
+                    for (int j = 0; j < sampleCount; j++)
+                    {
+                        float t2 = j / (float)(sampleCount - 1); // Normalized parameter along spline
+                        Vector3 p2 = spline.EvaluatePosition(t2);
+                        Vector3 d2 = spline.EvaluateTangent(t2);
+
+                        float distance = Vector3.Distance(p1, p2);
+
+
+
+                        float similarity = w_dist * Mathf.Pow(distance, 2) + Mathf.Abs(w_dir * Vector3.Dot(d1, d2)); 
+                        if (similarity < bestSimilarityPoint)
+                        {
+                            bestSimilarityPoint = similarity;
+                            if (i == 0)
+                            {
+                                currStartIndex = t2;
+                            }
+                            else if (i == sampleCount - 1)
+                            {
+                                currEndIndex = t2;
+                            }
+                            //bestPoint = i;
+                        }
+                    }
+
+                    splineSimilarity += bestSimilarityPoint;
+                    ///// END COMPARISON
+
+
+                    ///// COMPARISON AT EACH SECTION TO SECTION
+                    // Vector3 p2 = spline.EvaluatePosition(t1);
+                    // Vector3 d2 = spline.EvaluateTangent(t1);
+
+                    // float distance = Vector3.Distance(p1, p2);
+
+
+
+                    // float similarity = w_dist * Mathf.Pow(distance, 2) + Mathf.Abs(w_dir * Vector3.Dot(d1, d2)); 
+
+                    // splineSimilarity += similarity;
+                    ///// END COMPARISON
+
+
+                }
+                if (splineSimilarity < bestSimilaritySpline)
+                {
+                    bestSimilaritySpline = splineSimilarity;
+                    bestSpline = spline;
+                    bestIndex = index;
+                    splineStartIndex = currStartIndex;
+                    splineEndIndex = currEndIndex;
+                }
+            }
+            return bestSpline;
+        }
+
+        bool IsPointBetween(Vector3 point, Vector3 start, Vector3 end)
+        {
+            return (point.x >= Mathf.Min(start.x, end.x) && point.x <= Mathf.Max(start.x, end.x)) &&
+                (point.y >= Mathf.Min(start.y, end.y) && point.y <= Mathf.Max(start.y, end.y)) &&
+                (point.z >= Mathf.Min(start.z, end.z) && point.z <= Mathf.Max(start.z, end.z));
+        }
+
+        public void BeginMorph(Spline spline, int splineIndex, float splineStart, float splineEnd)
+        {
+
+            Vector3 splitStart = spline.EvaluatePosition(splineStart);
+            Vector3 splitEnd = spline.EvaluatePosition(splineEnd);
+            int startIndex = -1;
+            int endIndex = -1;
+            float bestStartDist = float.MaxValue;
+            float bestEndDist = float.MaxValue;
+            Spline newSpline = new Spline();
+
+            for (int i = 0; i < spline.Knots.Count(); i++)
+            {
+                var k = spline.Knots.ElementAt(i).Position;
+                var startDist = Vector3.Distance(k, splitStart);
+                var endDist = Vector3.Distance(k, splitEnd);
+
+                // If the split position is between two control points, we split here
+                if (startDist < bestStartDist)
+                {
+                    bestStartDist = startDist;
+                    startIndex = i;
+                }
+                if (startDist < bestEndDist)
+                {
+                    bestEndDist = endDist;
+                    endIndex = i;
+                }
+            }
+
+            if (startIndex == float.MaxValue || endIndex == float.MaxValue)
+            {
+                Debug.Log("Error with split");
+                Debug.Log("SPLIT INDEX1: " + startIndex + " SPLIT INDEX2: " +endIndex);
+
+                return;
+            }
+            else if (startIndex == endIndex)
+            {
+                endIndex = (endIndex + 1)%spline.Knots.Count();
+                Debug.Log("Split at same location");
+            }
+            int counter = 0;
+
+            for (int i = Mathf.Min(startIndex, endIndex); i <= Mathf.Max(startIndex, endIndex); i++)
+            {
+                newSpline.Add(spline.Knots.ElementAt(i));
+                counter ++;
+            }
+            Debug.Log("SPLIT INDEX1: " + startIndex + " SPLIT INDEX2: " +endIndex + " NUMBER OF KNOTS " +counter);
+            
+
+
+
+            m_SplineColoredContainer.AddSpline(newSpline);
+            m_SplineColoredContainer.GetComponent<SplineExtrude>().Rebuild();
+            // Delete spline from original container so it can't be selected a second time
+            // Will need to change them back when a reset occurs
+            // m_SplineContainer.RemoveSpline(spline);
+            // m_SplineContainer.GetComponent<SplineExtrude>().Rebuild();
+
+            // Group knots along spline and associate them with knots on the spline to morph into
+            // use Vector3.MoveTowards from the group of knots to the associated knot
+            // Once done, delete duplicate knots
+        }
 
         // TRANS-ROT-ARTWORK STATE CALLBACKS
 
@@ -182,6 +392,10 @@ namespace IVLab.MinVR3
             
             m_LastHandPos = handPosWorld;
             m_LastHandRot = handRotWorld;
+        }
+
+        public void TransRotArtwork_OnExit()
+        {
         }
 
 
@@ -241,6 +455,13 @@ namespace IVLab.MinVR3
         private Vector3 m_LastHandPos;
         private Quaternion m_LastHandRot;
         private Vector3 m_LastBrushPos;
+
+        // for stroke location testing
+        private List<Vector3> m_CurrentStrokeLocations;
+
+        [SerializeField] SplineContainer m_SplineContainer;
+
+        [SerializeField] SplineContainer m_SplineColoredContainer;
     }
 
 } // namespace
---
+++ Assets/Scripts/SplineMaker.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.Splines;
+using Unity.Mathematics;
+
+public class SplineMaker : MonoBehaviour
+{
+    [SerializeField] private Transform m_ArtworkParentTransform;
+    [SerializeField] public TextAsset testAssetData;
+    public SplineContainer m_splineContainer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ReadCSV(testAssetData);
+        m_splineContainer.transform.SetParent(m_ArtworkParentTransform, false);
+    }
+
+    void ReadCSV(TextAsset textAsset)
+    {
+        string[] data = textAsset.text.Split(new string[] { "\n" }, StringSplitOptions.None);
+
+        Spline spline = m_splineContainer.AddSpline();
+        List<BezierKnot> knots = new List<BezierKnot>();
+
+        int rowNum = 0;
+        bool firstEmpty = true;
+
+        foreach (string row in data)
+        {
+            // Ignore assumed header, should instead test the string itself instead of assuming header
+            if (rowNum != 0)
+            {
+                string[] rowValues = row.Split(new string[] { "," }, StringSplitOptions.None);
+
+                // Test that values exist in the row
+                if (rowValues[0].Length > 0)
+                {
+                    
+                    float integrationTime = float.Parse(rowValues[0]);
+                    float x = float.Parse(rowValues[1]);
+                    float y = float.Parse(rowValues[2]);
+                    float z = float.Parse(rowValues[3]);
+
+                    // Check if starting a new vector
+                    if (integrationTime == 0 && rowNum != 1)
+                    {
+                        spline.Knots = knots;
+                        SplineRange all = new SplineRange(0, spline.Count); // is not working to smooth, revisit
+                        spline.SetTangentMode(all, TangentMode.AutoSmooth);
+                        spline = m_splineContainer.AddSpline();
+                        knots = new List<BezierKnot>();
+                    }
+                    knots.Add(new BezierKnot(new float3(x, y, z)));
+                }
+                else if (firstEmpty)
+                {
+                    firstEmpty = false;
+                    spline.Knots = knots;
+                }
+            }  
+
+            rowNum ++;
+
+        }
+        m_splineContainer.GetComponent<SplineExtrude>().Rebuild();
+    }
+}
